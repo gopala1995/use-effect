e3c813b178cf97493fcbc8ea472a0bb5aa0a567b
Gopala <guptaraja727@gmail.com>
2022-02-03 01:46:08 +0530

    cc

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [data, setData] = useState();
+
+  console.log("1");
+  useEffect(() => {
+
+    fetch("http://localhost:3001/users")
+      .then((d) => d.json())
+      .then((res) => {
+        setData(res);
+        console.log("res:", res);
+      });
+    
+  },[]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {data.map((e) => (
+        <div>{e.name}</div>
+      ))}
     </div>
   );
 }
 
+
+
+//mounting : after HTML id shown on browser
+//updating : after state changed
+//unmounting: after HTML id removed from UI(browser)
 export default App;
